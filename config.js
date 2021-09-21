04c3755fdeb776df28eec222a2df649ebc2011b1
phatne1611 <86774242+phatne1611@users.noreply.github.com>
2021-09-21 19:56:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tiên Tiên à!',
+    introTitle: 'Minh Thư àaaa!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hoho',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu "Tiên" 😙',
+    btnIntro: 'haha',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích anh đó được chưa? <3',
-    btnNo: 'Không, Ngọc Phát à :3',
+    btnYes: 'Em thích anh  <3',
+    btnNo: 'Không, Anh à :3 ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Ngọc Phát <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối cho anh nắm tay em cái nhe.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Hẹn em hết dịch gặp nhau nhóooo.',
+    btnAccept: 'Dạaaaa <3',
+    messLink: 'http://fb.com' //https://www.facebook.com/messages/t/100052236047140/
 }
